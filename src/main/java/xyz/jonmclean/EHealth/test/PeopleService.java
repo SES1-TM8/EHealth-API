dc7f549c7c35162c6a08341d6de8adcef0738586
Jon McLean <13515869@student.uts.edu.au>
2020-04-29 18:11:34 +1000

    Added get endpoints for people service

--- src/main/java/xyz/jonmclean/EHealth/test/PeopleService.java
+++ src/main/java/xyz/jonmclean/EHealth/test/PeopleService.java
@@ -1,12 +1,19 @@
 package xyz.jonmclean.EHealth.test;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import xyz.jonmclean.EHealth.models.exceptions.UserNotFoundException;
 
 @Controller
 public class PeopleService {
@@ -27,6 +34,26 @@ public class PeopleService {
 		return people;
 	}
 	
+	@GetMapping("/people/all")
+	@ResponseBody
+	public List<People> getAllPeople() {
+		Iterable<People> iterablePeople = peopleRepo.findAll();
+		List<People> people = new ArrayList<People>();
+		iterablePeople.forEach(people::add);
+		return people;
+	}
 	
+	@GetMapping("/people/get/{id}")
+	@ResponseBody
+	public People getPerson(@PathVariable("id") long personId) throws UserNotFoundException {
+		Optional<People> optionalPerson = peopleRepo.findById(personId);
+		
+		if(!optionalPerson.isPresent()) {
+			throw new UserNotFoundException();
+		}
+		
+		People people = optionalPerson.get();
+		return people;
+	}
 	
 }

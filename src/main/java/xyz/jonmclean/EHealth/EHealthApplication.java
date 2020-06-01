c955aa8db376d1db12a6f451f814631235f748b7
Jon McLean <13515869@student.uts.edu.au>
2020-06-01 20:52:17 +1000

    Modified some config

--- src/main/java/xyz/jonmclean/EHealth/EHealthApplication.java
+++ src/main/java/xyz/jonmclean/EHealth/EHealthApplication.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
